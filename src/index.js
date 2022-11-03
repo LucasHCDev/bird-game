17ef01257102d5e70ee0c652d35ffcdbfcf84907
LucasHCDev <55065020+LucasHCDev@users.noreply.github.com>
2022-11-02 20:30:05 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,6 +11,19 @@ export const Game = makeSprite({
   },
 });
 
+export const gameProps = {
+  id: "Game",
+  size: {
+    width: 400,
+    height: 600,
+    maxHeightMargin: 150,
+  },
+  defaultFont: {
+    family: "Helvetica",
+    size: 24,
+  },
+};
+
 export const options = {
   dimensions: "scale-up",
 };
